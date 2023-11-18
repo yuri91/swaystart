529a8beaca913bb6c64b7bb066053b957eaffd9a
Yuri Iozzelli <y.iozzelli@gmail.com>
2023-11-18 17:06:40 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    naersk.url = "github:nmattia/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, naersk, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ rust-overlay.overlays.default ];
+    };
+    rust-build = pkgs.rust-bin.stable.latest.default.override {
+      extensions = [ "rust-src" ];
+      targets = [];
+    };
+    naersk-lib = naersk.lib.${system}.override {
+      rustc = rust-build;
+      cargo = rust-build;
+    };
+    swaystart = naersk-lib.buildPackage {
+      pname = "swaystart";
+      root = ./.;
+      buildInputs = with pkgs; [
+        glib
+      ];
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        rust-build
+      ];
+    };
+  in
+  {
+    devShell.${system} = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        cargo-edit
+        rust-analyzer-unwrapped
+      ];
+      inputsFrom = with pkgs; [
+        swaystart
+      ];
+      RUST_SRC_PATH = "${rust-build}/lib/rustlib/src/rust/library";
+    };
+    packages.${system}.default = swaystart;
+  };
+}
